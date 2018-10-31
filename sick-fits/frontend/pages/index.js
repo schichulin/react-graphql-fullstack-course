a957ba3ce3d794c3a83a5ed3b7cec5c23cc99887
Sergey Chichulin <schichulin@smartling.com>
2018-10-31 10:46:26 +0200

    create Nav with links to pages

--- sick-fits/frontend/pages/index.js
+++ sick-fits/frontend/pages/index.js
@@ -1,15 +1,6 @@
 // dev note: next.js takes care about the React importing
 // import React from "react";
 
-import Link from "next/link";
-
-const Home = () => (
-  <>
-    <h3>Hey Home!</h3>
-    <Link href="/sell">
-      <a>Go to Sell page</a>
-    </Link>
-  </>
-);
+const Home = () => <h3>Hey Home!</h3>;
 
 export default Home;
--- sick-fits/frontend/pages/sell.js
+++ sick-fits/frontend/pages/sell.js
@@ -1,12 +1,4 @@
-import Link from "next/link";
 
-const Sell = () => (
-  <div>
-    <h3>Sell smth!</h3>
-    <Link href="/">
-      <a>Go to Home page</a>
-    </Link>
-  </div>
-);
+const Sell = () => <h3>Sell smth!</h3>;
 
 export default Sell;
