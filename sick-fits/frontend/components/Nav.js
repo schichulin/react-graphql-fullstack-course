984d12f602b97e98130be2499e235ea731ac95b9
Sergey Chichulin <schichulin@smartling.com>
2018-12-09 18:39:31 +0200

    add loading progress indicator by NProgress

--- sick-fits/frontend/components/Nav.js
+++ sick-fits/frontend/components/Nav.js
@@ -1,5 +1,21 @@
+
 import Link from "next/link";
+import Router from "next/router";
+import NProgress from "nprogress";
+
 import NavStyles from "./styles/NavStyles";
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
 
 const Nav = () => (
   <NavStyles>
