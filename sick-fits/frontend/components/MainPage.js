81c2b16bf40a8dbcb54011672ba28910278e1656
Sergey Chichulin <schichulin@smartling.com>
2018-12-09 17:57:24 +0200

    initial styles by styled-components

--- sick-fits/frontend/components/MainPage.js
+++ sick-fits/frontend/components/MainPage.js
@@ -1,15 +1,40 @@
+
+import styled, { ThemeProvider, injectGlobal } from "styled-components";
 import Header from "./Header";
 import Meta from "./Meta";
+
+const theme = {
+  red: "#FF0000",
+  black: "#393939",
+  grey: "#3A3A3A",
+  lightgrey: "#E1E1E1",
+  offWhite: "#EDEDED",
+  maxWidth: "1000px",
+  bs: "0 12px 24px 0 rgba(0, 0, 0, 0.09)",
+};
+
+const StyledPage = styled.div`
+  background: white;
+  color: ${({ theme }) => theme.black};
+`;
+
+const Inner = styled.div`
+  max-width: ${({ theme }) => theme.maxWidth};
+  margin: 0 auto;
+  padding: 2rem;
+`;
 
 /**
  * Common page for all the router pages
  */
 const MainPage = (props) => (
-  <div>
-    <Meta />
-    <Header />
-    {props.children}
-  </div>
+  <ThemeProvider theme={theme}>
+    <StyledPage>
+      <Meta />
+      <Header />
+      <Inner>{props.children}</Inner>
+    </StyledPage>
+  </ThemeProvider>
 );
 
 export default MainPage;
